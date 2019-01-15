26199d804b9579795dc6760475df8ae48e0350d6
Raikish <albert.montagut.casero@gmail.com>
2019-01-15 20:55:59 +0100

    Adding hud scene and implementing on main scene

--- Main.gd
+++ Main.gd
@@ -9,13 +9,13 @@ var time_left
 var screensize
 var playing = false
 
+
 func _ready():
 	# random seed of the random generator
 	randomize()
 	screensize = get_viewport().get_visible_rect().size
 	$Player.screensize = screensize
 	$Player.hide()
-	new_game()
 
 
 func new_game():
@@ -27,6 +27,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 
 func spawn_coins():
@@ -45,4 +47,26 @@ func _process(delta):
 		spawn_coins()
 
 
+func _on_GameTimer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
 
+
+func _on_Player_pickup():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_Player_hurt():
+	game_over()
+
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	for coin in $CoinContainer.get_children():
+		coin.queue_free()
+	$HUD.show_game_over()
+	$Player.die()
